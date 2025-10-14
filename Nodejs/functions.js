179b02b51b22e2c02e52fd1222aa5b337ede076e
Alan Kennedy <Alaneduardokennedy@gmail.com>
2025-10-14 19:05:18 -0300

    new: rename and functions

---
+++ Nodejs/functions.js
@@ -0,0 +1,11 @@
+//FUNCIONES
+
+// Declarativa
+function nombreFunction(parametro1, parametro2) {
+    return parametro1 + parametro2;
+}
+
+// Expresión
+let nombreFunctionExpression = function(parametro1, parametro2) {
+    return parametro1 + parametro2;
+}
