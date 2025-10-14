06605929b1d2580ad1c63d1996585e1c2ba340d5
Alan Kennedy <Alaneduardokennedy@gmail.com>
2025-10-14 18:58:03 -0300

    new: arrays

--- Nodejs/clase1.js
+++ Nodejs/clase1.js
@@ -1,3 +1,6 @@
+// Javascript no es un lenguaje fuertemente tipado, por eso es que las variables
+// se pueden declarar sin un tipo de dato especifico.
+
 // Variables
 
 //Alcance de bloque, palabra reservada let
@@ -6,6 +9,8 @@ let edad;
 
 nombre = "Alan";
 edad = 21;
+
+nombre = 31; // No hay error, porque no es fuertemente tipado, pero no es buena práctica
 
 // Alcance global, palabra reservada var
 var pais = "Argentina";
@@ -25,3 +30,8 @@ let sinValor; // Valor por defecto es undefined. Indica que no tiene valor
 
 // Null
 let nulo = null; // Representa la ausencia intencional de cualquier valor
+
+// Arrays
+let paises =  [pais, "Brasil", "Chile", "Uruguay"];
+
+let arreglo = [numero, pais, cadena, verdadero]; // Arreglo heterogéneo, ya que JS es un lenguaje no tipado
