aeb2632c66620cb0c00c7cd34e6970fe4f0dc06b
Alan Kennedy <Alaneduardokennedy@gmail.com>
2025-10-14 18:28:04 -0300

    new: types

--- Nodejs/clase1.js
+++ Nodejs/clase1.js
@@ -1,11 +1,27 @@
 // Variables
 
-//Alcance de bloque
+//Alcance de bloque, palabra reservada let
 let nombre; 
 let edad;
 
 nombre = "Alan";
 edad = 21;
 
-// Alcance global
+// Alcance global, palabra reservada var
 var pais = "Argentina";
+
+//Tipos primitivos
+// Number
+let numero = 10; // Enteros o racionales. Mismas operaciones que matemática.
+
+// String 
+let cadena = "Hola mundo"; // Cadenas de caracteres
+
+// Boolean
+let verdadero = true; // Valores lógicos
+
+// Undefinded
+let sinValor; // Valor por defecto es undefined. Indica que no tiene valor
+
+// Null
+let nulo = null; // Representa la ausencia intencional de cualquier valor
